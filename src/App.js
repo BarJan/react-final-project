f91a32149864b497e507d137f985d64136c0d7b8
barjan <bardugo1@protonmail.com>
2021-01-11 23:31:13 +0200

    install and import parse

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Parse from 'parse';
 
 function App() {
+
+  const [activeUser, setActiveUser] = useState(
+    Parse.User.current() ? new UserModel(Parse.User.current()) : null);   // During development it's conveient to be logged in by default  
+
+  function handleLogout() {
+    setActiveUser(null);
+    Parse.User.logOut();
+  }
+
+  function handleLogin(loggedinUser) {
+    setActiveUser(loggedinUser);
+  }
+
+
   return (
     <HashRouter>
       <Switch>
         <Route exact path="/"><HomePage activeUser={activeUser} onLogout={handleLogout}/></Route>
+        <Route exact path="/UserHome"><UserHomePage activeUser={activeUser} onLogout={handleLogout}/></Route>
         <Route exact path="/login"><LoginPage activeUser={activeUser} users={users} onLogin={handleLogin}/></Route>
         <Route exact path="/dates"><DatesPage activeUser={activeUser} dates={activeUserDates} addDate={addDate}/></Route>
         <Route exact path="/profile"><PrfilePage activeUser={activeUser} onLogout={handleLogout} /></Route>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+Parse.initialize(
+  'FRZklTDYcQhNItVaX3Gz3vaTJmBLLFWUv4rJJ1dY', // This is your Application ID
+  'IboXQLNaP6F0nla3JzwdKk861oA18Z3kK3lJKCIL', // This is your Javascript key
+);
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />

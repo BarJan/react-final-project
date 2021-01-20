0789ac8fc08b4c5233a605ddaa87f9d9d0b3add4
barjan <bardugo1@protonmail.com>
2021-01-20 15:19:13 +0200

    added date-add to parse

--- src/components/AddDate/AddDate.js
+++ src/components/AddDate/AddDate.js
@@ -1,12 +1,36 @@
-import { Button, Modal } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
 import { ReactJewishDatePicker } from "react-jewish-datepicker";
-
+import Parse from 'parse';
 
 function AddDate(props) {
     const {show ,setShow, initialDate} = props;
+    const [dateDetails, setDateDetails] = useState("");
+    const [dateToAdd, setDateToAdd] = useState(new Date());
+
+
+    const date = Parse.Object.extend('date');
+    const myNewObject = new date();
+
+    function addDate() {
+        myNewObject.set('pickeDate', dateToAdd);
+        myNewObject.set('userId', Parse.User.current());
+        myNewObject.set('details', dateDetails);
+
+        myNewObject.save().then(
+            (result) => {
+                if (typeof document !== 'undefined') alert(`נוצר תאריך חדש!`);
+                    console.log('date created', result);
+                },
+                (error) => {
+                    if (typeof document !== 'undefined') alert(`אוי לא, משהו קרה :( נסי להזין תאריך חדש ליומן)`);
+                        console.error('Error while creating date: ', error);
+                }
+        );    
+    }
 
     return(
-        <Modal centered show={show} onHide={()=> setShow(false)}>
+        <Modal size="lg" centered show={show} onHide={()=> setShow(false)}>
                 <Modal.Header>
                     <Modal.Title>הוסיפי תאריך חדש</Modal.Title>
                 </Modal.Header>
@@ -15,16 +39,25 @@ function AddDate(props) {
                     <p>לחצי על התאריך לשינוי.</p>
                     <p>והוסיפי תיאור/פירוט לתאריך.</p>
                 </Modal.Body>
+
                 <ReactJewishDatePicker
                     value={initialDate}
                     isHebrew
                     onClick={(day) => {
-                        console.log(day);
+                        setDateToAdd(day.date);
                     }}
                     className="rjp"/>
+                
+                <Modal.Dialog>
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>תיאור/פירוט התאריך:</Form.Label>
+                        <Form.Control as="textarea" rows={3} cols={100} value={dateDetails} onChange={(e) => setDateDetails(e.target.value)}/>
+                    </Form.Group>
+                </Modal.Dialog>
+
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={()=> setShow(false)}>Cancel</Button>
-                    <Button variant="primary" onClick={()=> {setShow(false);}}>Delete</Button>
+                    <Button variant="secondary" onClick={()=> setShow(false)}>ביטול</Button>
+                    <Button variant="primary" onClick={()=> {addDate(); setShow(false);}}>הוסף ליומן</Button>
                 </Modal.Footer>
         </Modal>
     );

7d0856bf1e2807c8be0f503a7cb1a6f48b2251fb
barjan <bardugo1@protonmail.com>
2021-01-24 01:56:44 +0200

    improved calendar's algorithm

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -12,28 +12,63 @@ function Calendar(){
     const [showAddNewDate, setShowAddNewDate] = useState(false);
     const [addDateInit, setAddDateInit] = useState(new Date());
 
+    const translator = {
+        1: "א", 
+        2: "ב",
+        3: "ג",
+        4: "ד",
+        5: "ה",
+        6: "ו",
+        7: "ז",
+        8: "ח",
+        9: "ט",
+        10: "י",
+        11: "יא",
+        12: "יב",
+        13: "יג",
+        14: "יד",
+        15: "טו",
+        16: "טז",
+        17: "יז",
+        18: "יח",
+        19: "יט",
+        20: "כ",
+        21: "כא",
+        22: "כב",
+        23: "כג",
+        24: "כד",
+        25: "כה",
+        26: "כו",
+        27: "כז",
+        28: "כח",
+        29: "כט",
+        30: "ל"
+      }   
+
     useEffect(()=>{
         setDays();
     },[initDate]);
 
     function setDays(){
-        let firstOfMonthOffset = new Hebcal.HDate(1, initDate.getMonth()).getDay();
-        let daysInMonth = initDate.daysInMonth();
-        let daysInPastMonth = new Hebcal.HDate(1, initDate.getMonth()-1).daysInMonth()
+        let firstOfMonth =  new Hebcal.HDate(1, initDate.getMonth());
+        let daysInMonth = firstOfMonth.daysInMonth();
+        let firstOfMonthOffset = firstOfMonth.getDay();
+        let lastOfMonthOffset = 6-Hebcal.HDate(daysInMonth, firstOfMonth.getMonth()).getDay();
+        let daysInPastMonth = Hebcal.HDate(1, firstOfMonth.getMonth()-1).daysInMonth()
         let month = [];
         let daysAtMonth = [];
-        for(let i=1; i <= daysInMonth + firstOfMonthOffset; i++){
+        for(let i=1; i <= daysInMonth + firstOfMonthOffset + lastOfMonthOffset; i++){
             if(i<=firstOfMonthOffset)
-                month.push(daysInPastMonth+i-firstOfMonthOffset);
+                month.push(new Hebcal.HDate(daysInPastMonth+i-firstOfMonthOffset, firstOfMonth.getMonth()-1));
             else
-                month.push(i-firstOfMonthOffset);
+                month.push(new Hebcal.HDate(i-firstOfMonthOffset, firstOfMonth.getMonth()));
         }
-        daysAtMonth = month.map((day, index) => <Day key={index} day={day} isHebrew={true} onClick={setAddDate} past={index<firstOfMonthOffset? true : false}/>);
+        daysAtMonth = month.map((day, index) => <Day key={index} day={day} isHebrew={true} translator={translator} onClick={setAddDate} anotherMonth={index<firstOfMonthOffset? true : index>(daysInMonth+firstOfMonthOffset-1)? true : false}/>);
         setMonthDays(daysAtMonth);
     }
 
     function setAddDate(day) {
-        setAddDateInit(new Hebcal.HDate(day, initDate.getMonth(),initDate.getFullYear()).greg());
+        setAddDateInit(day.greg());
         setShowAddNewDate(true);
     }
 
--- src/components/Calendar/Day.js
+++ src/components/Calendar/Day.js
@@ -1,12 +1,36 @@
 import * as React from 'react';
-
+import Parse from 'parse';
 
 function Day(props) {
     
-    const {day, onClick, past} = props;
-    let showDay = day ? day : "";
+    const {day, isHebrew, translator, onClick, anotherMonth} = props;
+
+    let showDay = isHebrew ? translator[day.getDate()] : day.getDate();
+
+    function colorClassName() {
+        const date = Parse.Object.extend('date');
+        const query = new Parse.Query(date);
+        query.equalTo("pickeDate", new Date("2021-01-06T08:21:00.000Z"));
+        query.equalTo("userId", Parse.User.current());
+        query.find().then((results) => {
+        // You can use the "get" method to get the value of an attribute
+        // Ex: response.get("<ATTRIBUTE_NAME>")
+            console.log(results);
+            let dateCategory = results[0].get("dateCategory");
+            console.log(dateCategory);
+            if (dateCategory==="other"){
+                return dateCategory;
+            }
+           
+        }, (error) => {
+            return "";
+            // if (typeof document !== 'undefined')
+            //     console.error('Error while fetching date', error);
+          });
+    }
+
     return (
-        <div className={"day"+(past? " pastMonth" : "")} title={day} onClick={() => past? null : onClick(day)}>
+        <div className={"day"+(anotherMonth? " pastMonth" : "")+" "+colorClassName()} onClick={() => anotherMonth? null : onClick(day)} title={day.greg().toDateString().substring(4)+" "+day.toString(isHebrew?"h":null)}>
             {showDay}
         </div>
     )
